339819e4182053b74a5742b7dc829a07e5814473
SixpennyYard <mart1.boubou@gmail.com>
2024-10-28 19:14:04 +0100

    fix jump et checkpoint 1

--- Assets/Code/RiveB/Joueur/PlayerMovement.cs
+++ Assets/Code/RiveB/Joueur/PlayerMovement.cs
@@ -44,7 +44,7 @@ public class PlayerMovement : MonoBehaviour
             canJumpWall = true;
         }
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButton("Jump"))
         {
             if (isGrounded)
             {
---
+++ Assets/Code/RiveB/World/TileEffect/KeyTile.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Code.RiveB.World.TileEffect
+{
+    public class KeyTile : TileEffect
+    {
+        public override void ApplyEffect(Rigidbody2D playerRb)
+        {
+            SceneManager.LoadScene("RiveB_checkpoint1");
+        }
+    }
+}
